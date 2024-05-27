e1e2f0cf862ea058466d1cda314304fde9cf44d0
HanChenye1122 <1670439315@qq.com>
2024-05-27 23:57:20 +0800

    link to mysql

--- models.py
+++ models.py
@@ -3,15 +3,17 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    current_members = db.Column(db.Integer, default=0, nullable=False)
     destination = db.Column(db.String(150), nullable=False)
     max_members = db.Column(db.Integer, nullable=False)
-    current_members = db.Column(db.Integer, default=0)
     admin_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     admin = db.relationship('User', backref=db.backref('teams', lazy=True))
